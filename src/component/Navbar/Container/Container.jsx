9e7765148defdc52a435c970bf49c7d319a3aab8
mohamedmustafa21 <mohamedmeda023@gmail.com>
2023-02-23 20:18:29 +0200

    mu

---
+++ src/component/Navbar/Container/Container.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Container() {
+  return (
+    <div className='px-[200px]'>
+      
+    </div>
+  )
+}
+
+export default Container
---
+++ src/component/Navbar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+function NavBar() {
+  return (
+    <div className='px-5 grid-cols-12'>
+            <div className="right col-span-6">right</div>
+            <div className="left col-span-6">left</div>
+    </div>
+  )
+}
+
+export default NavBar
