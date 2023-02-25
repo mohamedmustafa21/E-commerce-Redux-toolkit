928cb4bf3df8f285566aab57170b84df582b8ff4
mohamedmustafa21 <mohamedmeda023@gmail.com>
2023-02-25 18:05:43 +0200

    mo

--- src/components/LandingPage/Featured/Featured.jsx
+++ src/components/LandingPage/Featured/Featured.jsx
@@ -8,9 +8,12 @@ import Product6 from "./imges/Untitled-17_1-removebg-preview.png";
 
 function Featured() {
   return (
-    <div className="grid grid-cols-12  bg-[#BEDCAB] px-5 gap-4 rounded-[15px] ">
-      <div className="relative col-span-2 max-md:col-span-6 bg-white px-5  flex justify-center flex-col text-center mt-4 mb-4 rounded-[15px]">
-        <img src={Product1} className="h-[50%] object-cover" alt="" />
+    <>
+    <h1 className="font-bold text-[25px] max-md:text-center">Featured</h1>
+      <div className="grid grid-cols-12  bg-[#BEDCAB] px-5 gap-4 rounded-[15px] ">
+     
+      <div className="relative max-md:p-2 col-span-2 max-md:col-span-12 bg-white px-5  flex justify-center flex-col text-center mt-4 mb-4 rounded-[15px]">
+        <img src={Product1} className="h-[50%]  object-cover " alt="" />
         <div className="product-deatils   ">
             <div className="rate text-[#EECA16]">
             <i class="fa-solid fa-star"></i>
@@ -23,10 +26,10 @@ function Featured() {
           <strong>EGP 19699.00</strong>
           
         </div>
-        <span className="absolute text-[white] bg-[#EECA16] top-[10%] left-0 p-2 w-[4rem]">10%</span>
+        <span className="absolute text-[white] max-md:opacity-[.5] bg-[#EECA16] top-[5%] max-md:w-[35%] rounded-r-[15px] left-0 p-2 w-[4rem]">10%</span>
       </div>
-      <div className="relative col-span-2 max-md:col-span-6 bg-white px-5  flex justify-center flex-col text-center mt-4 mb-4 rounded-[15px]">
-        <img src={Product1} className="h-[50%] object-cover" alt="" />
+      <div className="relative max-md:p-2 col-span-2 max-md:col-span-12 bg-white px-5  flex justify-center flex-col text-center mt-4 mb-4 rounded-[15px]">
+        <img src={Product2} className="h-[50%]  object-cover " alt="" />
         <div className="product-deatils   ">
             <div className="rate text-[#EECA16]">
             <i class="fa-solid fa-star"></i>
@@ -39,10 +42,10 @@ function Featured() {
           <strong>EGP 19699.00</strong>
           
         </div>
-        <span className="absolute text-[white] bg-[#EECA16] top-[10%] left-0 p-2 w-[4rem]">10%</span>
+        <span className="absolute text-[white] max-md:opacity-[.5] bg-[#EECA16] top-[5%] max-md:w-[35%] rounded-r-[15px] left-0 p-2 w-[4rem]">10%</span>
       </div>
-       <div className="relative col-span-2 max-md:col-span-6 bg-white px-5  flex justify-center flex-col text-center mt-4 mb-4 rounded-[15px]">
-        <img src={Product1} className="h-[50%] object-cover" alt="" />
+      <div className="relative max-md:p-2 col-span-2 max-md:col-span-12 bg-white px-5  flex justify-center flex-col text-center mt-4 mb-4 rounded-[15px]">
+        <img src={Product3} className="h-[50%]  object-cover " alt="" />
         <div className="product-deatils   ">
             <div className="rate text-[#EECA16]">
             <i class="fa-solid fa-star"></i>
@@ -55,10 +58,10 @@ function Featured() {
           <strong>EGP 19699.00</strong>
           
         </div>
-        <span className="absolute text-[white] bg-[#EECA16] top-[10%] left-0 p-2 w-[4rem]">10%</span>
+        <span className="absolute text-[white] max-md:opacity-[.5] bg-[#EECA16] top-[5%] max-md:w-[35%] rounded-r-[15px] left-0 p-2 w-[4rem]">10%</span>
       </div>
-      <div className="relative col-span-2 max-md:col-span-6 bg-white px-5  flex justify-center flex-col text-center mt-4 mb-4 rounded-[15px]">
-        <img src={Product1} className="h-[50%] object-cover" alt="" />
+      <div className="relative max-md:p-2 col-span-2 max-md:col-span-12 bg-white px-5  flex justify-center flex-col text-center mt-4 mb-4 rounded-[15px]">
+        <img src={Product4} className="h-[50%]  object-cover " alt="" />
         <div className="product-deatils   ">
             <div className="rate text-[#EECA16]">
             <i class="fa-solid fa-star"></i>
@@ -71,10 +74,10 @@ function Featured() {
           <strong>EGP 19699.00</strong>
           
         </div>
-        <span className="absolute text-[white] bg-[#EECA16] top-[10%] left-0 p-2 w-[4rem]">10%</span>
+        <span className="absolute text-[white] max-md:opacity-[.5] bg-[#EECA16] top-[5%] max-md:w-[35%] rounded-r-[15px] left-0 p-2 w-[4rem]">10%</span>
       </div>
-      <div className="relative col-span-2 max-md:col-span-6 bg-white px-5  flex justify-center flex-col text-center mt-4 mb-4 rounded-[15px]">
-        <img src={Product1} className="h-[50%] object-cover" alt="" />
+      <div className="relative max-md:p-2 col-span-2 max-md:col-span-12 bg-white px-5  flex justify-center flex-col text-center mt-4 mb-4 rounded-[15px]">
+        <img src={Product5} className="h-[50%]  object-cover " alt="" />
         <div className="product-deatils   ">
             <div className="rate text-[#EECA16]">
             <i class="fa-solid fa-star"></i>
@@ -87,10 +90,10 @@ function Featured() {
           <strong>EGP 19699.00</strong>
           
         </div>
-        <span className="absolute text-[white] bg-[#EECA16] top-[10%] left-0 p-2 w-[4rem]">10%</span>
+        <span className="absolute text-[white] max-md:opacity-[.5] bg-[#EECA16] top-[5%] max-md:w-[35%] rounded-r-[15px] left-0 p-2 w-[4rem]">10%</span>
       </div>
-      <div className="relative col-span-2 max-md:col-span-6 bg-white px-5  flex justify-center flex-col text-center mt-4 mb-4 rounded-[15px]">
-        <img src={Product1} className="h-[50%] object-cover" alt="" />
+      <div className="relative max-md:p-2 col-span-2 max-md:col-span-12 bg-white px-5  flex justify-center flex-col text-center mt-4 mb-4 rounded-[15px]">
+        <img src={Product6} className="h-[50%]  object-cover " alt="" />
         <div className="product-deatils   ">
             <div className="rate text-[#EECA16]">
             <i class="fa-solid fa-star"></i>
@@ -103,9 +106,11 @@ function Featured() {
           <strong>EGP 19699.00</strong>
           
         </div>
-        <span className="absolute text-[white] bg-[#EECA16] top-[10%] left-0 p-2 w-[4rem]">10%</span>
+        <span className="absolute text-[white] max-md:opacity-[.5] bg-[#EECA16] top-[5%] max-md:w-[35%] rounded-r-[15px] left-0 p-2 w-[4rem]">10%</span>
       </div>
     </div>
+    </>
+  
   );
 }
 
