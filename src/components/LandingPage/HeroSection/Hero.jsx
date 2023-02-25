4f242d4751300f79a8ab973e29230f3a57a6288e
mohamedmustafa21 <mohamedmeda023@gmail.com>
2023-02-25 12:28:04 +0200

    mo

--- src/components/LandingPage/HeroSection/Hero.jsx
+++ src/components/LandingPage/HeroSection/Hero.jsx
@@ -1,29 +1,33 @@
 import React from "react";
 import HeroImg from "../../../Assets/Hero.png";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper";
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from "swiper";
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/autoplay";
 
 function Hero() {
   return (
     <div>
       <Swiper
-       modules={[Navigation, Pagination]}
-       spaceBetween={50}
-       slidesPerView={1}
-       navigation
-       pagination={{ clickable: true }}
-       scrollbar={{ draggable: true }}
-      
-      controller={true}
-      autoplay={true}
-        freeMode = {true}
-       
+        modules={[Autoplay, Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        centeredSlides={true}
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: false,
+        }}
+        loop
+        pagination={{ clickable: true }}
+        // scrollbar={{ draggable: true }}
+        onSlideChange={() => console.log("slide change")}
+        controller={true}
+        freeMode={true}
         className="mySwiper w-[100%] "
       >
         <SwiperSlide className="relative">
@@ -33,8 +37,12 @@ function Hero() {
             alt="image slide 1"
           />
           <div className="absolute top-[40%] right-[70%] max-md:top-[40%] max-md:right-[10%] ">
-            <h1 className="font-[700] text-[25px]">Up To  <br /> <span className="text-[35px]"> 50% OFF</span></h1>
-          <button className="bg-[#A3BE23] font-[700] text-[#FEFBFB] p-2 rounded-[10px] text-[15px]">SHOP NOW</button>
+            <h1 className="font-[700] text-[25px]">
+              Up To <br /> <span className="text-[35px]"> 50% OFF</span>
+            </h1>
+            <button className="bg-[#A3BE23] font-[700] text-[#FEFBFB] p-2 rounded-[10px] text-[15px]">
+              SHOP NOW
+            </button>
           </div>
         </SwiperSlide>
         <SwiperSlide className="relative">
@@ -44,8 +52,12 @@ function Hero() {
             alt="image slide 1"
           />
           <div className="absolute top-[40%] right-[70%] max-md:top-[40%] max-md:right-[10%] ">
-            <h1 className="font-[700] text-[25px]">Up To  <br /> <span className="text-[35px]"> 50% OFF</span></h1>
-          <button className="bg-[#A3BE23] font-[700] text-[#FEFBFB] p-2 rounded-[10px] text-[15px]">SHOP NOW</button>
+            <h1 className="font-[700] text-[25px]">
+              Up To <br /> <span className="text-[35px]"> 50% OFF</span>
+            </h1>
+            <button className="bg-[#A3BE23] font-[700] text-[#FEFBFB] p-2 rounded-[10px] text-[15px]">
+              SHOP NOW
+            </button>
           </div>
         </SwiperSlide>
         <SwiperSlide className="relative">
@@ -55,8 +67,12 @@ function Hero() {
             alt="image slide 1"
           />
           <div className="absolute top-[40%] right-[70%] max-md:top-[40%] max-md:right-[10%] ">
-            <h1 className="font-[700] text-[25px]">Up To  <br /> <span className="text-[35px]"> 50% OFF</span></h1>
-          <button className="bg-[#A3BE23] font-[700] text-[#FEFBFB] p-2 rounded-[10px] text-[15px]">SHOP NOW</button>
+            <h1 className="font-[700] text-[25px]">
+              Up To <br /> <span className="text-[35px]"> 50% OFF</span>
+            </h1>
+            <button className="bg-[#A3BE23] font-[700] text-[#FEFBFB] p-2 rounded-[10px] text-[15px]">
+              SHOP NOW
+            </button>
           </div>
         </SwiperSlide>
       </Swiper>
