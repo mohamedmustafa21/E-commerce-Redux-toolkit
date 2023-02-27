92c5d71d6d6a312b788b4a495f68a4e5cf0d3615
xLe0x <ammarmosaber@gmail.com>
2023-02-27 21:34:13 +0200

    done

--- src/components/Global/Container.jsx
+++ src/components/Global/Container.jsx
@@ -1,6 +1,6 @@
 function Container({ children }) {
   return (
-    <div className="px-8 sm:max-w-xl md:max-w-4xl lg:max-w-5xl mx-auto">
+    <div className="px-8 sm:max-w-2xl md:max-w-5xl lg:max-w-7xl mx-auto">
       {children}
     </div>
   );
--- src/components/Global/Footer.jsx
+++ src/components/Global/Footer.jsx
@@ -2,20 +2,16 @@ import Logo from "../../assets/imgs/Logos/logo-pngwhite.png";
 
 function Footer() {
   return (
-    <div className='w-full h-[35vh] bg-[#2E4620] p-5  '>
-      <div className='flex justify-around max-md:justify-between max-md:items-center py-5  max-md:relative'>
-         <div className='p-5 mt-5  max-md:absolute max-md:top-[50%] max-md:left-0 '>
-        <img className='w-32 max-md:w-16' src={Logo}  alt="" />
-    <div className="w-full bg-[#2E4620] p-5 ">
+    <div className="w-full bg-[#2E4620] p-5">
       <div className="grid grid-cols-2 sm:grid-cols-10 mx-auto py-5 place-items-stretch max-md:relative ">
-        <div className="col-span-1 sm:col-span-2 p-5 mt-5  sm:w-64 hidden sm:block max-md:absolute max-md:top-[50%] max-md:left-0">
+        <div className="col-span-1 sm:col-span-2 px-5 mt-5 sm:w-64 hidden lg:block max-md:absolute max-md:top-[50%] max-md:left-0">
           <img className="w-full" src={Logo} alt="" />
         </div>
         <div className="px-5 py-2 col-span-1 sm:col-span-2">
-          <h1 className="text-[white] text-[20px] font-[500] max-md:text-[15px]">
+          <h1 className="text-[white] text-xl font-[500] max-md:text-2xl mb-2">
             RESOURCES
           </h1>
-          <ul className="text-[#e4e3e3] text-[15px] font-[300] max-md:text-[12px]">
+          <ul className="text-[#e4e3e3] text-lg font-[300] max-md:text-base">
             <li>Application</li>
             <li>Documentation</li>
             <li>Systems</li>
@@ -23,20 +19,20 @@ function Footer() {
           </ul>
         </div>
         <div className="px-5 py-2 col-span-1 sm:col-span-2">
-          <h1 className="text-[white] text-[20px] font-[500] max-md:text-[15px]">
+          <h1 className="text-[white] text-xl font-[500] max-md:text-2xl mb-2">
             PRICING
           </h1>
-          <ul className="text-[#e4e3e3] text-[15px] font-[300] max-md:text-[12px]">
+          <ul className="text-[#e4e3e3] text-lg font-[300] max-md:text-base">
             <li>Overview</li>
             <li>Premium Plans</li>
             <li>Affiliate Program</li>
           </ul>
         </div>
         <div className="px-5 py-2 col-span-1 sm:col-span-2">
-          <h1 className="text-[white] text-[20px] font-[500] max-md:text-[15px]">
+          <h1 className="text-[white] text-xl font-[500] max-md:text-2xl mb-2">
             COMPANY
           </h1>
-          <ul className="text-[#e4e3e3] text-[15px] font-[300] max-md:text-[12px]">
+          <ul className="text-[#e4e3e3] text-lg font-[300] max-md:text-base">
             <li>About Us</li>
             <li>Blog</li>
             <li>Partnerships</li>
@@ -45,19 +41,16 @@ function Footer() {
           </ul>
         </div>
         <div className="px-5 py-2 col-span-1 sm:col-span-2">
-          <h1 className="text-[white] text-[20px] font-[500] max-md:text-[15px]">
+          <h1 className="text-[white] text-xl font-[500] max-md:text-2xl mb-2">
             SOCIAL
           </h1>
-          <ul className="text-[#e4e3e3] text-[15px] font-[300] max-md:text-[12px]">
+          <ul className="text-[#e4e3e3] text-lg font-[300] max-md:text-base">
             <li>Facebook</li>
             <li>Instagram</li>
             <li>LinkedIn</li>
           </ul>
         </div>
       </div>
-    </div>
-    </div>
-    </div>
     </div>
   );
 }
